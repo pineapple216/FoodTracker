6ab2d21fe084c5f24bd9f25ab94288106b496a10
Koen Hendriks <koenhendriks89@gmail.com>
2015-03-27 15:12:58 +0100

    Basic storyboard setup

---
+++ FoodTracker/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  FoodTracker
+//
+//  Created by Koen Hendriks on 27/03/15.
+//  Copyright (c) 2015 Koen Hendriks. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+	@IBOutlet weak var textView: UITextView!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+	@IBAction func eatItBarButtonItemPressed(sender: UIBarButtonItem) {
+	}
+
+	
+
+}
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource, UISearchBarDelegate, UISearchControllerDelegate, UISearchResultsUpdating {
 
+	@IBOutlet weak var tableView: UITableView!
+	
+	var searchController:UISearchController!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,32 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	// MARK: UITableViewDataSource
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
 
+	// MARK: UISearchResultsUpdating
+	
+	func updateSearchResultsForSearchController(searchController: UISearchController) {
+		
+	}
+	
 
 }
+
+
+
+
+
+
+
+
+
 
