f1be5e8cbba9cf5c353455b629a564bc7ae7b0e6
Koen Hendriks <koenhendriks89@gmail.com>
2015-04-01 20:51:48 +0200

    Implemented search functionality

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
 	
 	var searchController:UISearchController!
 	
+	var suggestedSearchFoods:[String] = []
+	var filteredSuggestedSearchFoods:[String] = []
+	
+	var scopeButtonTitles = ["Recommended","Search Results","Saved"]
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -22,13 +27,19 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
 		self.searchController.searchResultsUpdater = self
 		self.searchController.dimsBackgroundDuringPresentation = false
 		self.searchController.hidesNavigationBarDuringPresentation = false
+		
+		// Set the size and position of the searchbar
 		self.searchController.searchBar.frame = CGRectMake(self.searchController.searchBar.frame.origin.x, self.searchController.searchBar.frame.origin.y, self.searchController.searchBar.frame.size.width, 44.0)
 		
 		self.tableView.tableHeaderView = self.searchController.searchBar
+		
+		self.searchController.searchBar.scopeButtonTitles = scopeButtonTitles
 		self.searchController.searchBar.delegate = self
 		
 		self.definesPresentationContext = true
 		
+		// Array with suggested foods
+		self.suggestedSearchFoods = ["apple", "bagel", "banana", "beer", "bread", "carrots", "cheddar cheese", "chicen breast", "chili with beans", "chocolate chip cookie", "coffee", "cola", "corn", "egg", "graham cracker", "granola bar", "green beans", "ground beef patty", "hot dog", "ice cream", "jelly doughnut", "ketchup", "milk", "mixed nuts", "mustard", "oatmeal", "orange juice", "peanut butter", "pizza", "pork chop", "potato", "potato chips", "pretzels", "raisins", "ranch salad dressing", "red wine", "rice", "salsa", "shrimp", "spaghetti", "spaghetti sauce", "tuna", "white wine", "yellow cake"]
 		
 	}
 
@@ -40,17 +51,48 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
 	// MARK: UITableViewDataSource
 	
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 0
+		// Determine how many rows we want to display in our tableView based wether the user is using the search bar or not
+		if self.searchController.active{
+			return self.filteredSuggestedSearchFoods.count
+		}
+		else{
+			return self.suggestedSearchFoods.count
+		}
 	}
 	
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-		return UITableViewCell()
+		let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+		var foodName:String
+		
+		if self.searchController.active{
+			foodName = filteredSuggestedSearchFoods[indexPath.row]
+		}
+		else{
+			foodName = suggestedSearchFoods[indexPath.row]
+		}
+		
+		cell.textLabel?.text = foodName
+		cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+		
+		return cell
 	}
 
 	// MARK: UISearchResultsUpdating
 	
+	// Function to update the search controllers results
+	// is called everytime we change something in the search bar
 	func updateSearchResultsForSearchController(searchController: UISearchController) {
-		
+		let searchString = self.searchController.searchBar.text
+		let selectedScopeButtonIndex = self.searchController.searchBar.selectedScopeButtonIndex
+		self.filterContentForSearch(searchString, scope: selectedScopeButtonIndex)
+		self.tableView.reloadData()
+	}
+	
+	func filterContentForSearch(searchText: String, scope: Int){
+		self.filteredSuggestedSearchFoods = self.suggestedSearchFoods.filter({ (food : String) -> Bool in
+			var foodMatch = food.rangeOfString(searchText)
+			return foodMatch != nil
+		})
 	}
 	
 
